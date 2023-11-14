e924531080c482c39f2458ac6fe495457084d4db
Diaz <dhiazcandra1@gmail.com>
2023-11-14 14:41:09 +0700

    custom component

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -15,22 +15,36 @@ class HomePage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Container(
-            margin: EdgeInsets.all(8),
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            decoration: BoxDecoration(
-                color: Colors.green
-            ),
-            child: Text("Persegi"),
+          CustomMenu(imageAssets: "assets/persegi.png", title: "persegi"),
+          CustomMenu(imageAssets: "assets/segitiga.jpg", title: "segitiga"),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomMenu extends StatelessWidget {
+  const CustomMenu({
+    super.key,
+    required this.imageAssets,
+    required this.title,
+  });
+
+  final String imageAssets;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(color: Colors.green),
+      child: Column(
+        children: [
+          Image.asset(
+            imageAssets,
           ),
-          Container(
-            margin: EdgeInsets.all(8),
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            decoration: BoxDecoration(
-                color: Colors.green
-            ),
-            child: Text("Segitiga"),
-          )
+          Text(title),
         ],
       ),
     );
